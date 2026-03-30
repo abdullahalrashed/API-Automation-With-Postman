e252a3f5bde38be37be1440c86993e7e9c922d23
abdullahalrashed <raashare1@gmail.com>
2026-03-30 12:58:00 +0600

    forloop while loop

--- Java Fundamentals.js
+++ Java Fundamentals.js
@@ -36,3 +36,34 @@ else
 }
 
 let i=0
+while(i>7)
+{
+    console.log(i)
+}
+
+do
+{
+i++
+}while(i>10);
+console.log(i)
+// 2 and   5 
+//  from 1 to 10 give me common multiple values of 2 and 5
+console.log("***********************")
+let n =0
+for(let k=1;k<=100;k++)
+{
+    if(k%2 == 0 && k%5 == 0)
+    {
+        n++
+    console.log(k)
+    if(n ==3)
+    break
+    }
+}
+
+required = true
+while(required)
+{
+console.log(required)
+required= false
+}
