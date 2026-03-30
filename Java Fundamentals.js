eaba5145412bd647b8946a21d50899ff2a7d576a
abdullahalrashed <raashare1@gmail.com>
2026-03-30 21:03:15 +0600

    Function

--- Java Fundamentals.js
+++ Java Fundamentals.js
@@ -170,3 +170,36 @@ console.log(fruits.reverse())
 var scores = [12,14,15,16,17]
 console.log(scores.sort((a,b) => a-b)) // ascending order
 console.log(scores.sort((a,b) => b-a)) // descending order
+
+//block of code 
+//var - global level/functional
+//let global level/block level {}
+//const -
+const greet = "Evening"
+//greet = "night"
+
+if( 1 ==1)
+{
+    let greet = "Afternoon"
+}
+
+function add(a,b)
+{
+    let greet = "Morning"
+    return a+b
+}
+
+let sum =add(2,3)
+console.log(sum)
+console.log(greet)
+
+//do not have name =>Anyonymus function-- Function expressions
+
+let sumOfIntegers = function(c,d)
+{
+    return c+d
+}
+
+let sumOfNumbers= (c,d)=> c+d
+
+console.log(sumOfNumbers(2,3))
