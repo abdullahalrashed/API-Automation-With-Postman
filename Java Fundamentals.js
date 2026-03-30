6de23cf987e8f4ae83be9d234544ff2effdc9c7c
abdullahalrashed <raashare1@gmail.com>
2026-03-30 21:19:36 +0600

    String Manupulation

--- Java Fundamentals.js
+++ Java Fundamentals.js
@@ -203,3 +203,23 @@ let sumOfIntegers = function(c,d)
 let sumOfNumbers= (c,d)=> c+d
 
 console.log(sumOfNumbers(2,3))
+
+//strings Manupulation
+
+  const Person = require('./basics7')
+let day = 'tuesday '
+console.log(day.length)  //8
+let subDay = day.slice(0,4)
+console.log(subDay)
+console.log(day[1]) //u
+//tue   day 
+let splitDay =day.split("s")
+console.log(splitDay[1].length)
+console.log(splitDay[1].trim().length)
+
+
+let date = '23'
+let nextDate = '27'
+let diff = parseInt(nextDate) - parseInt(date)
+console.log(diff)
+diff.toString()
